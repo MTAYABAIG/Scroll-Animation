9b83bd6758eb289f1cd918195b6f208d37f287d7
Muhammad Tayyab Baig <116713172+MTAYABAIG@users.noreply.github.com>
2023-06-07 12:23:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll',checkBoxes)
+checkBoxes();
+
+function checkBoxes(){
+    const triggerpoint = window.innerHeight/5*4;
+    boxes.forEach(box =>{
+        const boxtop = box.getBoundingClientRect().top;
+
+        if(boxtop < triggerpoint){
+            box.classList.add('show');
+        }
+        else{
+            box.classList.remove('show');
+        }
+    })
+}
